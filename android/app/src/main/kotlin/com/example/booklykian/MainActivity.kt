047e831eadf6428a9ee0729edb7ec345d3e92e25
mac <yunis192002@gmail.com>
2024-07-14 08:03:38 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/booklykian/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.booklykian
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:booklykian/features/splash/presentation/views/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
